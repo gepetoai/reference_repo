fee40a3fbd681b3af0fa3ee378488a04bf5f11d8
uqarni <uqarni@gmail.com>
2024-12-19 14:53:04 +0500

    testing instructor

--- gepeto.py
+++ gepeto.py
@@ -2,40 +2,43 @@ import requests
 
 class Gepeto:
     def __init__(self):
+        #return a list of agents
+        #return org-specific api keys 
+        #return database / resource variables
+        #return globl variables
         pass
 
 class Agent:
     def __init__(self, name):
-        self.name = name
-    
-    def get_info(self):  
         try:
-            headers = {"API-Key": "test-key"}  # Changed from "api_key" to "API-Key"
+            self.name = name
+            headers = {"API-Key": "test-key"}  
             response = requests.get(f"http://0.0.0.0:8001/api/v1/agents/{self.name}", headers=headers)
             self.llm = response.json().get("llm")
             self.model = response.json().get("model")
             self.prompt = response.json().get("prompt")
             self.response_model = response.json().get("response_model")
-        
+
         except Exception as e:
             print(e)
 
+
     def async_run(self):
         pass
+
 
     def run(self):
         info = self.get_info()
         print("got info: ", info)
         print(f"Agent {self.name} is running")
 
-
-        if llm == "openai":
+        if self.llm == "openai": 
             #impleent openai client 
             pass
 
-        elif llm == "anthropic":
+        elif self.llm == "anthropic":
             #impleent anthropic client
             pass
 
         else:
-            raise ValueError(f"Model {llm} not supported")
+            raise ValueError(f"Model {self.llm} not supported")
